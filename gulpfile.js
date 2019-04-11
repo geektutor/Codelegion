772b739370826e594bec146320f3d802c1cfc7d9
geektutor <sodiq.akinjobi@gmail.com>
2019-04-11 12:30:37 +0100

    Pushed the first instance

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,136 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
+"use strict";
 
-// ***********************
-// Move JS Files to src/js
-// ***********************
+// Load plugins
+const autoprefixer = require("gulp-autoprefixer");
+const browsersync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
+const gulp = require("gulp");
+const header = require("gulp-header");
+const merge = require("merge-stream");
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const uglify = require("gulp-uglify");
 
-gulp.task('js', function () {
-    return gulp.src([
-      'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/bootstrap/dist/js/bootstrap.min.js',
-      'node_modules/popper.js/dist/umd/popper.min.js',
-      'node_modules/owl.carousel2/dist/owl.carousel.min.js',
-      'node_modules/magnific-popup/dist/jquery.magnific-popup.min.js',
-      'node_modules/wowjs/dist/wow.min.js',
-      'node_modules/imagesloaded/imagesloaded.pkgd.min.js',
-      'node_modules/isotope-layout/dist/isotope.pkgd.min.js',
-      'node_modules/jarallax/dist/jarallax.min.js',
-      'node_modules/jarallax/dist/jarallax-video.min.js',
-      'node_modules/jquery-waypoints/waypoints.min.js',
-      'node_modules/counterup/jquery.counterup.min.js',
-      'node_modules/jquery.easing/jquery.easing.min.js',
-      'node_modules/jquery-countdown/dist/jquery.countdown.min.js'
+// Load package.json for banner
+const pkg = require('./package.json');
+
+// Set the banner content
+const banner = ['/*!\n',
+  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' */\n',
+  '\n'
+].join('');
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Clean vendor
+function clean() {
+  return del(["./vendor/"]);
+}
+
+// Bring third party dependencies from node_modules into vendor directory
+function modules() {
+  // Bootstrap JS
+  var bootstrap = gulp.src('./node_modules/bootstrap/dist/js/**/*')
+    .pipe(gulp.dest('./vendor/bootstrap/js'));
+  // Font Awesome
+  var fontAwesome = gulp.src('./node_modules/@fortawesome/**/*')
+    .pipe(gulp.dest('./vendor'));
+  // jQuery Easing
+  var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
+    .pipe(gulp.dest('./vendor/jquery-easing'));
+  // Magnific Popup
+  var magnificPopup = gulp.src('./node_modules/magnific-popup/dist/*')
+    .pipe(gulp.dest('./vendor/magnific-popup'));
+  // jQuery
+  var jquery = gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
     ])
-        .pipe(gulp.dest("src/js"))
-});
+    .pipe(gulp.dest('./vendor/jquery'));
+  return merge(bootstrap, fontAwesome, jquery, jqueryEasing, magnificPopup);
+}
 
-// *****************************
-// Combine All js files into one
-// *****************************
+// CSS task
+function css() {
+  return gulp
+    .src("./scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: "expanded",
+      includePaths: "./node_modules",
+    }))
+    .on("error", sass.logError)
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest("./css"))
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("./css"))
+    .pipe(browsersync.stream());
+}
 
-gulp.task('scripts', function () {
-    return gulp.src([
-    './src/js/default-assets/avoid.console.error.js',
-    './src/js/default-assets/classy-nav.js',
-    'node_modules/owl.carousel2/dist/owl.carousel.min.js',
-    'node_modules/wowjs/dist/wow.min.js',
-    'node_modules/magnific-popup/dist/jquery.magnific-popup.min.js',
-    'node_modules/jquery-waypoints/waypoints.min.js',
-    'node_modules/counterup/jquery.counterup.min.js',
-    'node_modules/imagesloaded/imagesloaded.pkgd.min.js',
-    'node_modules/isotope-layout/dist/isotope.pkgd.min.js',
-    'node_modules/jarallax/dist/jarallax.min.js',
-    'node_modules/jarallax/dist/jarallax-video.min.js',
-    './src/js/default-assets/jquery.scrollup.min.js',
-    'node_modules/jquery-countdown/dist/jquery.countdown.min.js'
-  ])
-        .pipe(concat('confer.bundle.js'))
-        .pipe(gulp.dest('./src/js/'))
-});
-
-// ************************************
-// Move Font Awesome Fonts to src/fonts
-// ************************************
-
-gulp.task('fafonts', function () {
-    return gulp.src([
-      'node_modules/font-awesome/fonts/*'
+// JS task
+function js() {
+  return gulp
+    .src([
+      './js/*.js',
+      '!./js/*.min.js'
     ])
-        .pipe(gulp.dest('src/fonts'))
-})
+    .pipe(uglify())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./js'))
+    .pipe(browsersync.stream());
+}
 
-// *******************************
-// Move Elegent Icons to src/fonts
-// *******************************
-gulp.task('elefonts', function () {
-    return gulp.src([
-      'node_modules/elegant-icons/fonts/*'
-    ])
-        .pipe(gulp.dest('src/css/fonts'))
-})
+// Watch files
+function watchFiles() {
+  gulp.watch("./scss/**/*", css);
+  gulp.watch("./js/**/*", js);
+  gulp.watch("./**/*.html", browserSyncReload);
+}
 
-// *******************************
-// Move ZMDI Icons to src/fonts
-// *******************************
-gulp.task('zmdifonts', function () {
-    return gulp.src([
-      'node_modules/material-design-iconic-font/dist/fonts/*'
-    ])
-        .pipe(gulp.dest('src/fonts'))
-})
+// Define complex tasks
+const vendor = gulp.series(clean, modules);
+const build = gulp.series(vendor, gulp.parallel(css, js));
+const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
-// *******************
-// Move CSS to src/css
-// *******************
-
-gulp.task('css', function () {
-    return gulp.src([
-      'node_modules/bootstrap/dist/css/bootstrap.min.css',
-      'node_modules/font-awesome/css/font-awesome.min.css',
-      'node_modules/owl.carousel2/dist/assets/owl.carousel.min.css',
-      'node_modules/elegant-icons/style.css',
-      'node_modules/wowjs/css/libs/animate.css',
-      'node_modules/magnific-popup/dist/magnific-popup.css',
-      'node_modules/material-design-iconic-font/dist/css/material-design-iconic-font.min.css'
-    ])
-        .pipe(gulp.dest('src/css'))
-})
-
-gulp.task('default', ['js', 'scripts', 'css', 'fafonts', 'elefonts', 'zmdifonts']);
+// Export tasks
+exports.css = css;
+exports.js = js;
+exports.clean = clean;
+exports.vendor = vendor;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
